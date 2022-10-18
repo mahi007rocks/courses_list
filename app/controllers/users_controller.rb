0a6fea599d26d684c416b2846bcb963b78bc1c3f
Mahesh <gmahesh007babu@gmail.com>
2022-10-18 16:01:57 +0530

    Intial commit and user model, controller aded

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_user, only: [:create]
+    before_action :find_user, only: [:show, :update, :destroy]
+
+    def index
+      @users = User.all
+      render json: @users, status: 200
+    end
+
+    def show
+      render json: @user, status: 200
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: 201
+      else
+        render json: { erors: @user.errors.full_messages }, status: 503
+      end
+    end
+
+    def update
+      unless @user.update(user_params)
+        render json: {erors: @user.errors.full messages}, status: 503
+      end
+    end
+
+    def destroy
+      @user.destroy
+    end
+
+    private
+      def user_params
+        params.permit(:username, :email, :password)
+      end
+
+      def find_user
+        @user = User.find(params[:id])
+      end
+end
